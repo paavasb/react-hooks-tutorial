bcff1f5c98868e1cc0e80626ff66cc2a8bf189bd
Paavas Bhasin <paavas22@gmail.com>
2020-04-25 14:17:06 +0530

    Lecture 200

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,19 @@
-import React, { useContext } from 'react';
-import NotesContext from '../context/notes-context';
+import React, { useContext } from 'react'
+import NotesContext from '../context/notes-context'
+import useMousePosition from '../hooks/useMousePosition';
 
 const Note = (props) => {
-    const { dispatch } = useContext(NotesContext);
+    const { dispatch } = useContext(NotesContext)
+    const position = useMousePosition()
+
     return (
       <div>
         <h3>{props.note.title}</h3>
         <p>{props.note.body}</p>
+        <p>{position.x}, {position.y}</p>
         <button onClick={() => dispatch({ type: 'REMOVE_NOTE', title: props.note.title })}>x</button>
       </div>
     )
 }
 
-export default Note;
+export default Note
